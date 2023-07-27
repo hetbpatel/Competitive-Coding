d398ecff2f14b65a5072a08655d9c8ff2908cb3c
PATEL HET BHUPENDRABHAI <124247624+hetbpatel@users.noreply.github.com>
2023-07-27 16:24:55 +0530

    Update 0076-minimum-window-substring.cpp

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/minimum-window-substring/solutions/26808/here-is-a-10-line-template-that-can-solve-most-substring-problems/
+
 class Solution {
 public:
     string minWindow(string s, string t) {
