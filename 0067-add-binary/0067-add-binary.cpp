73423bdfe71d0db1cded87b553855f28ea98d74a
PATEL HET BHUPENDRABHAI <124247624+hetbpatel@users.noreply.github.com>
2023-07-27 16:06:10 +0530

    Update 0067-add-binary.cpp

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -21,3 +21,32 @@ public:
         return res;
     }
 };
+
+/*-------------------------------------------------------------------------------------------------------*/
+
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int n = a.size();
+        int m = b.size();
+        int carry=0;
+        int i=n-1;
+        int j=m-1;
+        string ans="";
+
+        while(i>=0 or j>=0 or carry)
+        {
+            if(i>=0)    carry+=a[i]-'0';
+            if(j>=0)    carry+=b[j]-'0';
+        
+            ans+=carry%2+'0';
+            carry/=2;
+
+            i--;j--;
+        }
+
+        reverse(ans.begin(),ans.end());
+
+        return ans;
+    }
+};
