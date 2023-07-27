de4e8890091f1af6cf2659d0237877c1f132f83e
PATEL HET BHUPENDRABHAI <124247624+hetbpatel@users.noreply.github.com>
2023-07-27 16:00:54 +0530

    Update 0064-minimum-path-sum.cpp

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/minimum-path-sum/description/
+
 // https://www.youtube.com/watch?v=_rgTlyky1uQ&ab_channel=takeUforward
 // There are three basic approaches Reccursion,Memoizatiom,Tabulation
 // 1.Reccursion
@@ -67,5 +69,40 @@ int f(int i,int j,vector<vector<int>>& grid,vector<vector<int>> &dp){
             }
         }
         return dp[n-1][m-1];
+    }
+};
+
+/*-----------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+
+        int n = grid.size();
+        int m = grid[0].size();
+
+        int dp[n][m];
+        dp[0][0] = grid[0][0];
+
+        for(int i=1;i<m;i++)
+        {
+            dp[0][i] = grid[0][i] + dp[0][i-1];
+        }
+
+        for(int i=1;i<n;i++)
+        {
+            dp[i][0] = grid[i][0] + dp[i-1][0];
+        }
+
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<m;j++)
+            {
+                dp[i][j] = grid[i][j] + min(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+
+        return dp[n-1][m-1];
+        
     }
 };
