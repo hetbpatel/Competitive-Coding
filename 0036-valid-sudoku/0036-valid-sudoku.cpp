1768f6f58e361ef27639c5172197f6ad9a084424
PATEL HET BHUPENDRABHAI <124247624+hetbpatel@users.noreply.github.com>
2023-07-27 13:50:12 +0530

    Update 0036-valid-sudoku.cpp

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -29,3 +29,30 @@ public:
         return true;
     }
 };
+
+/*----------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int row[9][9] = {0};
+        int col[9][9] = {0};
+        int box[9][9] = {0};
+
+        for(int i=0;i<board.size();i++)
+        {
+            for(int j=0;j<board[i].size();j++)
+            {
+                if(board[i][j]!='.')
+                {
+                    int n = board[i][j] - '0' - 1;
+                    int k = 3*(i/3) + j/3;
+                    if(row[i][n]!=0 || col[j][n]!=0 || box[k][n]!=0)    return false;
+                    else    row[i][n] = col[j][n] = box[k][n] = 1;
+                }
+            }
+        }
+
+        return true;
+    }
+};
