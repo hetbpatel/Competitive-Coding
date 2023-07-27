f2c8aa984e30a95e0704a400e9487c44a1d76e81
PATEL HET BHUPENDRABHAI <124247624+hetbpatel@users.noreply.github.com>
2023-07-27 14:13:06 +0530

    Update 0045-jump-game-ii.cpp

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,4 +1,5 @@
 // https://www.youtube.com/watch?v=wLPdkLM_BWo&ab_channel=CodewithAlisha
+// https://github.com/hetbpatel/Love-Babbar-450/blob/master/14_DP/27_min_jump_to_reach_end.cpp
 class Solution {
 public:
     int jump(vector<int>& nums) {
